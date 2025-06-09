4b6d920808580374b8c8b6537f3813e51137e649
Emeka Ifeora <34845973+Emyboy@users.noreply.github.com>
2025-06-09 15:52:49 +0100

    feat: restructure application by removing unused components and implementing HomePage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,12 @@
 import { Route, Routes } from "react-router-dom";
-
-import IndexPage from "@/pages/index";
-import DocsPage from "@/pages/docs";
-import PricingPage from "@/pages/pricing";
-import BlogPage from "@/pages/blog";
-import AboutPage from "@/pages/about";
-import PWAUpdateNotification from "@/components/pwa-update-notification";
+import PWAUpdateNotification from "./components/pwa-update-notification";
+import HomePage from "./pages/home/HomePage";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route element={<IndexPage />} path="/" />
-        <Route element={<DocsPage />} path="/docs" />
-        <Route element={<PricingPage />} path="/pricing" />
-        <Route element={<BlogPage />} path="/blog" />
-        <Route element={<AboutPage />} path="/about" />
+        <Route element={<HomePage />} path="/" />
       </Routes>
       <PWAUpdateNotification />
     </>
