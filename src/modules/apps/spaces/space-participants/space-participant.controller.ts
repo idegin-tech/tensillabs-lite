608152b60f6544cc6912454c276866a20f042c20
Emeka Ifeora <34845973+Emyboy@users.noreply.github.com>
2025-10-31 01:10:15 +0100

    refactor: remove unnecessary ESLint disable comments from controller files

--- src/modules/apps/spaces/space-participants/space-participant.controller.ts
+++ src/modules/apps/spaces/space-participants/space-participant.controller.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
 import {
   Controller,
   Post,
--- src/modules/apps/spaces/tasks/services/task.service.ts
+++ src/modules/apps/spaces/tasks/services/task.service.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
